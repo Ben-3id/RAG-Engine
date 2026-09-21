9f198adb23fcaf78e328cc5b6d7a7da0434b6717
Ben3id <ae7666000@gmail.com>
2026-09-21 16:50:37 +0300

    fix bug database testing workflow

--- testing/integration_testing/connection_testing.py
+++ testing/integration_testing/connection_testing.py
@@ -5,32 +5,22 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(PROJECT_ROOT))
 
 
-
+import os
 import pytest 
 from src.settings.config import get_settings
 from sqlalchemy.ext.asyncio import create_async_engine 
 
 url =  "postgresql+asyncpg://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@pgvector:{DB_PORT}/{DB_NAME}"
 
-@pytest.fixture
-def config():
-    return get_settings()
-
-def test_config(config):
-    assert config is not None
 
 @pytest.fixture
-def engine(config):
-    POSTGRES_USERNAME = config.POSTGRES_USERNAME
-    POSTGRES_PASSWORD = config.POSTGRES_PASSWORD
-    DB_PORT = config.DB_PORT
-    DB_NAME = config.DB_NAME
+def engine():
 
     connection = url.format(
-        POSTGRES_USERNAME = POSTGRES_USERNAME ,
-        POSTGRES_PASSWORD = POSTGRES_PASSWORD ,
-        DB_PORT = DB_PORT ,
-        DB_NAME = DB_NAME 
+        POSTGRES_USERNAME = os.environ["POSTGRES_USERNAME"] ,
+        POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"] ,
+        DB_PORT = os.environ["DB_PORT"] ,
+        DB_NAME = os.environ["DB_NAME"] 
     )
 
     engine = create_async_engine(connection)
